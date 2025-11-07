8ebaad2aafe700ec2ad78b4e30d01d1cb4d8e0b5
jo-hseo <ellejo1110@gmail.com>
2025-11-07 15:38:44 +0900

    first commit

---
+++ src/main/java/com/sparta/deliverypathservice/DeliveryPathServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.deliverypathservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryPathServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryPathServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/deliverypathservice/DeliveryPathServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.deliverypathservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryPathServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
