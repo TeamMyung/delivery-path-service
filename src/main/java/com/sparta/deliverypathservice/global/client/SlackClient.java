7e2ed4fa62e7f5bae7469b880a99a548e18b4b34
jo-hseo <ellejo1110@gmail.com>
2025-11-11 22:22:20 +0900

    fix(delivery-path): SlackClient 수정

--- src/main/java/com/sparta/deliverypathservice/global/client/SlackClient.java
+++ src/main/java/com/sparta/deliverypathservice/global/client/SlackClient.java
@@ -9,12 +9,12 @@ import java.util.UUID;
 public interface SlackClient {
 
     //배송담당자에게 '배송 생성됨' 슬랙 메세지 전송
-    @PostMapping("/v1/slacks/api/send-message")
-    void sendMessage(Long userId, String text);
+    @PostMapping("/v1/slacks")
+    void sendMessage(String slackId, String text);
 
     //허브담당자에게 '배송 생성됨' 슬랙 메세지(ai) 전송
-    @PostMapping("/v1/slacks/api/send-message")
-    void sendMessage(UUID finalHubId, String text);
+//    @PostMapping("/v1/slacks/api/send-message")
+//    void sendMessage(UUID finalHubId, String text);
 
     //수령인에게 '배송 완료' 슬랙 알림
 }
