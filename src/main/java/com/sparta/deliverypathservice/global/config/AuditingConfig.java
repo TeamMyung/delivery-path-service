0727800e15e8d2b6a63eb123090555c7646da1fa
jo-hseo <ellejo1110@gmail.com>
2025-11-12 21:00:57 +0900

    add(global): 전체 Eureka, Swagger 추가

--- src/main/java/com/sparta/deliverypathservice/global/config/AuditingConfig.java
+++ src/main/java/com/sparta/deliverypathservice/global/config/AuditingConfig.java
@@ -1,16 +1,7 @@
 package com.sparta.deliverypathservice.global.config;
 
-import com.sparta.deliverypathservice.global.domain.user.User;
-import lombok.Getter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-
-import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
